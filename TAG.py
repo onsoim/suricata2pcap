7e436362bc6310d42a5eaa2776ec9baabb82519c
onsoim <onsoim@gmail.com>
2020-01-27 23:33:59 +0900

    common tags

--- TAG.py
+++ TAG.py
@@ -9,14 +9,6 @@ def findall(str, sub):
     
 
 class TAG:
-    def msg(value):
-        # print("msg :", value)
-        pass
-
-    def flow(value):
-        # print("flow :", value)
-        pass
-
     def content(value):
         value = value.strip('"')
         delimiter_bar = findall(value, '|')
@@ -32,222 +24,188 @@ class TAG:
                 except:
                     if delimiter_bar[i+1] < len(value) - 1:
                         c += bytes(value[delimiter_bar[i+1] + 1 : len(value) - 1], "utf-8")
-        print(f'content "{value}" -> {c}')
-        # return c
+        # print(f'content "{value}" -> {c}')
+        return {'content': c}
 
-    def within(value):
-        # print("within :", value)
-        pass
+    def flow(value):    return {'flow': value.strip().split(',')}
+    def within(value):  return {'within': value.strip()}
+    def sid(value):     return {'sid': value}
 
-    def reference(value):
-        # print("reference :", value)
-        pass
+    def classtype(value):   pass
+    def metadata(value):    pass
+    def msg(value):         pass
+    def reference(value):   pass
+    def rev(value):         pass
 
-    def classtype(value):
-        # print("classtype :", value)
-        pass
-
-    def sid(value):
-        # print("sid :", value)
-        pass
-
-    def rev(value):
-        # print("rev :", value)
-        pass
-
-    def metadata(value):
-        # print("metadata :", value)
-        pass
 
     def flowbits(value):
-        # print("flowbits :", value)
+        print("flowbits :", value)
         pass
     
     def threshold(value):
-        # print("threshold :", value)
+        print("threshold :", value)
         pass
     
     def flags(value):
-        # print("flags :", value)
+        print("flags :", value)
         pass
 
     def depth(value):
-        # print("depth :", value)
+        print("depth :", value)
         pass
     
     def distance(value):
-        # print("distance :", value)
+        print("distance :", value)
         pass
     
     def pcre(value):
-        # print("pcre :", value)
+        print("pcre :", value)
         pass
 
     def isdataat(value):
-        # print("isdataat :", value)
+        print("isdataat :", value)
         pass
 
     def byte_test(value):
-        # print("byte_test :", value)
+        print("byte_test :", value)
         pass
 
     def byte_jump(value):
-        # print("byte_jump :", value)
+        print("byte_jump :", value)
         pass
 
     def offset(value):
-        # print("offset :", value)
+        print("offset :", value)
         pass
 
     def dsize(value):
-        # print("dsize :", value)
+        print("dsize :", value)
         pass
 
     def itype(value):
-        # print("itype :", value)
+        print("itype :", value)
         pass
 
     def icode(value):
-        # print("icode :", value)
+        print("icode :", value)
         pass
 
     def tag(value):
-        # print("tag :", value)
+        print("tag :", value)
         pass
 
     def fast_pattern(*value):
-        # print("fast_pattern :", value)
+        print("fast_pattern :", value)
         pass
 
     def asn1(value):
-        # print("asn1 :", value)
+        print("asn1 :", value)
         pass
 
     def uricontent(value):
-        # print("uricontent :", value)
+        print("uricontent :", value)
         pass
 
     def urilen(value):
-        # print("urilen :", value)
+        print("urilen :", value)
         pass
 
     def ip_proto(value):
-        # print("ip_proto :", value)
+        print("ip_proto :", value)
         pass
         
     def detection_filter(value):
-        # print("detection_filter :", value)
+        print("detection_filter :", value)
         pass
 
     def icmp_id(value):
-        # print("icmp_id :", value)
+        print("icmp_id :", value)
         pass
 
     def byte_extract(value):
-        # print("byte_extract :", value)
+        print("byte_extract :", value)
         pass
 
     def ssl_version(value):
-        # print("ssl_version :", value)
+        print("ssl_version :", value)
         pass
 
     def ssl_state(value):
-        # print("ssl_state :", value)
+        print("ssl_state :", value)
         pass
 
     def softwareversion(value):
-        # print("softwareversion :", value)
+        print("softwareversion :", value)
         pass
 
     def stream_size(value):
-        # print("stream_size :", value)
+        print("stream_size :", value)
         pass
 
     def id(value):
-        # print("id :", value)
+        print("id :", value)
         pass
 
     def ssh_softwareversion(value):
-        # print("ssh_softwareversion :", value)
+        print("ssh_softwareversion :", value)
         pass
-
-    # def (value):
-    #     # print(" :", value)
-    #     pass
-
-
-    #############################
-    ###### standalone tags ######
-    #############################
 
     def ftpbounce():
-        # print("ftpbounce")
-        pass
-
-    def nocase():
-        # print("nocase")
+        print("ftpbounce")
         pass
 
     def http_user_agent():
-        # print("http_user_agent")
-        pass
-
-    def http_uri():
-        # print("http_uri")
+        print("http_user_agent")
         pass
 
     def http_header():
-        # print("http_header")
+        print("http_header")
         pass
 
     def http_method():
-        # print("http_method")
+        print("http_method")
         pass
 
     def http_host():
-        # print("http_host")
+        print("http_host")
         pass
 
     def http_client_body():
-        # print("http_client_body")
+        print("http_client_body")
         pass
 
     def http_raw_header():
-        # print("http_raw_header")
+        print("http_raw_header")
         pass
 
     def file_data():
-        # print("file_data")
+        print("file_data")
         pass
 
     def http_stat_code():
-        # print("http_stat_code")
+        print("http_stat_code")
         pass
 
     def http_raw_uri():
-        # print("http_raw_uri")
+        print("http_raw_uri")
         pass
 
     def http_cookie():
-        # print("http_cookie")
+        print("http_cookie")
         pass
 
     def dns_query():
-        # print("dns_query")
+        print("dns_query")
         pass
 
     def http_stat_msg():
-        # print("http_stat_msg")
+        print("http_stat_msg")
         pass
 
     def http_server_body():
-        # print("http_server_body")
+        print("http_server_body")
         pass
 
     def rawbytes():
-        # print("rawbytes")
+        print("rawbytes")
         pass
-
-    # def ():
-    #     # print("")
-    #     pass
--- s2p.py
+++ s2p.py
@@ -10,11 +10,9 @@ if __name__ == "__main__":
     for rule in rules:
         # raw_rule = re.findall(r'([^()]+)$', rule)
         # raw_header = raw_rule[0].split(" ")
-        # pcap = PCAP(raw_header[1], raw_header[2], raw_header[3], raw_header[5], raw_header[6])
-        # print(pcap.__dict__)
-
         delimiter_parentheses = rule.find('(')
-        raw_header, raw_tags = rule[:delimiter_parentheses - 1], rule[delimiter_parentheses + 1: -1]
+        raw_header, raw_tags = rule[:delimiter_parentheses - 1].split(' '), rule[delimiter_parentheses + 1: -1]
+        pcap = PCAP(raw_header[1], raw_header[2], raw_header[3], raw_header[5], raw_header[6])
 
         # regex without pcre
         # tags = [ tag.group(0) for tag in re.finditer(r'([\w.]{1,})(:("[^"]*"|[^;]*);)?', raw_tags) ]
@@ -28,11 +26,30 @@ if __name__ == "__main__":
             try:
                 delimiter_colon = tag.find(':')
                 if delimiter_colon + 1:
-                    k,v = tag[:delimiter_colon], tag[delimiter_colon + 1:]
+                    k, v = tag[:delimiter_colon], tag[delimiter_colon + 1:]
                     delimiter_dot = k.find('.')
                     if delimiter_dot != -1: k = k.replace('.', '_')
-                    TAG.__dict__[k](v)
-                else: TAG.__dict__[tag]()
+                    ret = TAG.__dict__[k](v)
+                    # if ret == None: print(k)
+                    if ret != None:
+                        k, v = list(ret.keys())[0], list(ret.values())[0]
+                        if k == 'content':
+                            pcap.__dict__[k].append(v)
+                        elif k == 'flow':
+                            pcap.__dict__['flow'] = v
+                        elif k == 'sid':
+                            pcap.__dict__['sid'] = v
+                        elif k == 'within':
+                            c = pcap.__dict__['content'][-1]
+                            pcap.__dict__['content'][-1] = b'A' * (int(v) - len(c)) + c
+                        else:
+                            print(k, v)
+
+                else:
+                    # ret = TAG.__dict__[tag]()
+                    if tag == 'http_uri':
+                        pcap.__dict__['http_uri'].append(pcap.__dict__['content'][-1])
+                        del pcap.__dict__['content'][-1]
 
             except KeyError:
                 print(f'unsupported keyword : {tag} - {rule}')
@@ -41,3 +58,5 @@ if __name__ == "__main__":
                 print(f'Value Error : {tags} {rule}')
 
             except Exception as e: print(e, tag)
+
+        print(pcap.__dict__)

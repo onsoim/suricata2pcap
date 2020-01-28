44fb815b6f73bda3580d9ade9840667582c6033f
onsoim <46339009+onsoim@users.noreply.github.com>
2020-01-28 09:22:36 +0900

    Content branch to Develop branch (#2)
    * working on content feature
    
    * convert content format strings to normal strings
    
    * common tags

--- PCAP.py
+++ PCAP.py
@@ -8,14 +8,24 @@ class PCAP:
         self.dst_ip = dst_ip
         self.dst_port = src_port
 
+        self.content = []
+        self.flow = []
+        self.http_uri = []
+        self.sid = 1000000
+
         self.gen_ip()
         self.gen_port()
     
 
     def gen_ip(self):
         if (self.src_ip == "any"): self.src_ip = self.random_ip()
+        elif (self.src_ip == "$HOME_NET"): self.src_ip = "192.168.0.1"
+        elif (self.src_ip == "$EXTERNAL_NET"): self.src_ip = "20.0.0.1"
+
         if (self.dst_ip == "any"): self.dst_ip = self.random_ip()
-        
+        elif (self.dst_ip == "$HOME_NET"): self.dst_ip = "192.168.0.1"
+        elif (self.dst_ip == "$EXTERNAL_NET"): self.dst_ip = "20.0.0.1"
+
     def random_ip(self):
         return ':'.join([ str(random.randint(0,255)) for _ in range(4) ])
 
--- s2p.py
+++ s2p.py
@@ -3,41 +3,60 @@ from TAG import *
 
 import re
 
-with open('rules/full_ruleset.rules', 'r') as r:
-    rules = r.read().splitlines()
+if __name__ == "__main__":
+    with open('rules/test.rules', 'r') as r:
+        rules = r.read().splitlines()
 
-for rule in rules:
-    # raw_rule = re.findall(r'([^()]+)$', rule)
-    # raw_header = raw_rule[0].split(" ")
-    # pcap = PCAP(raw_header[1], raw_header[2], raw_header[3], raw_header[5], raw_header[6])
-    # print(pcap.__dict__)
+    for rule in rules:
+        # raw_rule = re.findall(r'([^()]+)$', rule)
+        # raw_header = raw_rule[0].split(" ")
+        delimiter_parentheses = rule.find('(')
+        raw_header, raw_tags = rule[:delimiter_parentheses - 1].split(' '), rule[delimiter_parentheses + 1: -1]
+        pcap = PCAP(raw_header[1], raw_header[2], raw_header[3], raw_header[5], raw_header[6])
 
-    delimiter_parentheses = rule.find('(')
-    raw_header, raw_tags = rule[:delimiter_parentheses - 1], rule[delimiter_parentheses + 1: -1]
+        # regex without pcre
+        # tags = [ tag.group(0) for tag in re.finditer(r'([\w.]{1,})(:("[^"]*"|[^;]*);)?', raw_tags) ]
+        tags = [ tag.group(0)[:-1] for tag in re.finditer(r'([\w.]{2,})(:("[^"]*"|[^;\\]*))?;', raw_tags) ]
+        escape = raw_tags
+        for tag in tags: escape =  escape.replace(f'{tag};', '')
+        if escape.strip() != '': tags.append(escape.strip())
 
-    # regex without pcre
-    # tags = [ tag.group(0) for tag in re.finditer(r'([\w.]{1,})(:("[^"]*"|[^;]*);)?', raw_tags) ]
-    tags = [ tag.group(0)[:-1] for tag in re.finditer(r'([\w.]{2,})(:("[^"]*"|[^;\\]*))?;', raw_tags) ]
-    escape = raw_tags
-    for tag in tags: escape =  escape.replace(f'{tag};', '')
-    if escape.strip() != '': tags.append(escape.strip())
+        for tag in tags:
+            # try: globals()[k](v)
+            try:
+                delimiter_colon = tag.find(':')
+                if delimiter_colon + 1:
+                    k, v = tag[:delimiter_colon], tag[delimiter_colon + 1:]
+                    delimiter_dot = k.find('.')
+                    if delimiter_dot != -1: k = k.replace('.', '_')
+                    ret = TAG.__dict__[k](v)
+                    # if ret == None: print(k)
+                    if ret != None:
+                        k, v = list(ret.keys())[0], list(ret.values())[0]
+                        if k == 'content':
+                            pcap.__dict__[k].append(v)
+                        elif k == 'flow':
+                            pcap.__dict__['flow'] = v
+                        elif k == 'sid':
+                            pcap.__dict__['sid'] = v
+                        elif k == 'within':
+                            c = pcap.__dict__['content'][-1]
+                            pcap.__dict__['content'][-1] = b'A' * (int(v) - len(c)) + c
+                        else:
+                            print(k, v)
 
-    for tag in tags:
-        # try: globals()[k](v)
-        try:
-            delimiter_colon = tag.find(':')
-            if delimiter_colon + 1:
-                k,v = tag[:delimiter_colon], tag[delimiter_colon + 1:]
-                delimiter_dot = k.find('.')
-                if delimiter_dot != -1: k = k.replace('.', '_')
-                TAG.__dict__[k](v)
-            else:
-                TAG.__dict__[tag]()
+                else:
+                    # ret = TAG.__dict__[tag]()
+                    if tag == 'http_uri':
+                        pcap.__dict__['http_uri'].append(pcap.__dict__['content'][-1])
+                        del pcap.__dict__['content'][-1]
 
-        except KeyError:
-            print(f'unsupported keyword : {tag} - {rule}')
+            except KeyError:
+                print(f'unsupported keyword : {tag} - {rule}')
 
-        except ValueError:
-            print(f'Value Error : {tags} {rule}')
+            except ValueError:
+                print(f'Value Error : {tags} {rule}')
 
-        except Exception as e: print(e, tag)
+            except Exception as e: print(e, tag)
+
+        print(pcap.__dict__)
